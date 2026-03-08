fe197c265b2b305c132af57133a3da05a63c3810
ghesyarhegyta <ghesyarhegyta@gmail.com>
2026-03-08 16:25:35 +0800

    Add files via upload

---
+++ POSTTEST_2/POSTTEST_2/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtJudul = New System.Windows.Forms.TextBox()
+        Me.txtPenulis = New System.Windows.Forms.TextBox()
+        Me.txtGenre = New System.Windows.Forms.TextBox()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.lstBuku = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtJudulHapus = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 36)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Judul Buku"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 86)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Penulis"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 142)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 18)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Genre"
+        '
+        'txtJudul
+        '
+        Me.txtJudul.Location = New System.Drawing.Point(168, 27)
+        Me.txtJudul.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtJudul.Name = "txtJudul"
+        Me.txtJudul.Size = New System.Drawing.Size(124, 27)
+        Me.txtJudul.TabIndex = 3
+        '
+        'txtPenulis
+        '
+        Me.txtPenulis.Location = New System.Drawing.Point(168, 77)
+        Me.txtPenulis.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtPenulis.Name = "txtPenulis"
+        Me.txtPenulis.Size = New System.Drawing.Size(124, 27)
+        Me.txtPenulis.TabIndex = 4
+        '
+        'txtGenre
+        '
+        Me.txtGenre.Location = New System.Drawing.Point(168, 133)
+        Me.txtGenre.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtGenre.Name = "txtGenre"
+        Me.txtGenre.Size = New System.Drawing.Size(124, 27)
+        Me.txtGenre.TabIndex = 5
+        '
+        'btnTambah
+        '
+        Me.btnTambah.Location = New System.Drawing.Point(305, 186)
+        Me.btnTambah.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(94, 26)
+        Me.btnTambah.TabIndex = 6
+        Me.btnTambah.Text = "Tambah Buku"
+        Me.btnTambah.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(306, 186)
+        Me.btnHapus.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(94, 26)
+        Me.btnHapus.TabIndex = 7
+        Me.btnHapus.Text = "Hapus Buku"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'lstBuku
+        '
+        Me.lstBuku.FormattingEnabled = True
+        Me.lstBuku.ItemHeight = 18
+        Me.lstBuku.Location = New System.Drawing.Point(285, 314)
+        Me.lstBuku.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.lstBuku.Name = "lstBuku"
+        Me.lstBuku.Size = New System.Drawing.Size(399, 166)
+        Me.lstBuku.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("High Tower Text", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Crimson
+        Me.Label4.Location = New System.Drawing.Point(331, 25)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(352, 27)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Manage your lovely books here ˚. ᵎᵎ"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Mongolian Baiti", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Crimson
+        Me.Label5.Location = New System.Drawing.Point(359, 287)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(263, 24)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "─────────────୨ৎ─────────────"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Arial Narrow", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.LavenderBlush
+        Me.Label6.Location = New System.Drawing.Point(109, 167)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(177, 33)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "(๑'ᵕ'๑)⸝* byebye"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtJudul)
+        Me.GroupBox1.Controls.Add(Me.txtPenulis)
+        Me.GroupBox1.Controls.Add(Me.txtGenre)
+        Me.GroupBox1.Controls.Add(Me.btnTambah)
+        Me.GroupBox1.Font = New System.Drawing.Font("Mongolian Baiti", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.Crimson
+        Me.GroupBox1.Location = New System.Drawing.Point(58, 66)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(407, 218)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Tambah Buku"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.GroupBox2.Controls.Add(Me.txtJudulHapus)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.btnHapus)
+        Me.GroupBox2.Font = New System.Drawing.Font("Mongolian Baiti", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.ForeColor = System.Drawing.Color.Crimson
+        Me.GroupBox2.Location = New System.Drawing.Point(521, 66)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(407, 218)
+        Me.GroupBox2.TabIndex = 13
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Hapus Buku"
+        '
+        'txtJudulHapus
+        '
+        Me.txtJudulHapus.Location = New System.Drawing.Point(204, 33)
+        Me.txtJudulHapus.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.txtJudulHapus.Name = "txtJudulHapus"
+        Me.txtJudulHapus.Size = New System.Drawing.Size(124, 27)
+        Me.txtJudulHapus.TabIndex = 14
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(28, 39)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(89, 18)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Judul Buku"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Pink
+        Me.ClientSize = New System.Drawing.Size(1000, 506)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lstBuku)
+        Me.Font = New System.Drawing.Font("Mongolian Baiti", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "Form1"
+        Me.Text = "Mini Library Manager"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtJudul As TextBox
+    Friend WithEvents txtPenulis As TextBox
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents lstBuku As ListBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtJudulHapus As TextBox
+    Friend WithEvents Label7 As Label
+End Class
---
+++ POSTTEST_2/POSTTEST_2/ModuleData.vb
@@ -0,0 +1,8 @@
+﻿Module ModuleData
+
+    Public judulBuku(100) As String
+    Public penulisBuku(100) As String
+    Public genreBuku(100) As String
+    Public jumlahData As Integer = 0
+
+End Module
