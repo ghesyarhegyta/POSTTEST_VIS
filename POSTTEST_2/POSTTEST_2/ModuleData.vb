917a93771f7fae1952be3269f51b13fefaa329f1
ghesyarhegyta <ghesyarhegyta@gmail.com>
2026-03-08 16:26:51 +0800

    Delete POSTTEST_2 directory

--- POSTTEST_2/POSTTEST_2/ModuleData.vb
+++
@@ -1,8 +0,0 @@
-﻿Module ModuleData
-
-    Public judulBuku(100) As String
-    Public penulisBuku(100) As String
-    Public genreBuku(100) As String
-    Public jumlahData As Integer = 0
-
-End Module
